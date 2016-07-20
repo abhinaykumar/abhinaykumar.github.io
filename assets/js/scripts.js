fe571a1963b63e9f04a837b8a5c6e0ed95f180b3
Abhinay Kumar <abhinay302@gmail.com>
2016-07-20 23:50:44 +0530

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,15 +1,5 @@
 
 jQuery(document).ready(function() {
-	
-	/*
-    	Background slideshow
-	*/
-	$.backstretch([
-	  "assets/img/backgrounds/1.jpg", 
-	  "assets/img/backgrounds/2.jpg", 
-	  "assets/img/backgrounds/3.jpg"
-	  ], {duration: 3000, fade: 750});
-	
 	/*
 	    Contact form
 	*/
